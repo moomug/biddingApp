675757022596a5c260da74e9d21d42f77060302d
moomug <moghrabi.eng@outlook.com>
2026-04-14 21:12:15 +0200

    Final cleanup: aggregate removal of all bin and obj folders

--- Backend/MazadService/Entities/Mzad.cs
+++ Backend/MazadService/Entities/Mzad.cs
@@ -4,5 +4,14 @@ namespace MazadService.Entities;
 
 public class Mzad
 {
-
+ public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighTender { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime MzadEnd { get; set; } = DateTime.UtcNow;
+    public Status Status { get; set; }
+    public Horse Horse { get; set; }
 }
